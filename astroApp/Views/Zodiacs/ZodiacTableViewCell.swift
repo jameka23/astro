73cd3f0656bf526ab1336ded91bd32ee794847dd
Jameka Echols <jameka.echols@gmail.com>
2024-02-18 20:43:16 -0500

    added zodiactableviewcell

--- astroApp/Views/Zodiacs/ZodiacTableViewCell.swift
+++ astroApp/Views/Zodiacs/ZodiacTableViewCell.swift
@@ -10,5 +10,53 @@ import UIKit
 class ZodiacTableViewCell: UITableViewCell {
     static let reuseIdentifier = "ZodiacCell"
     
+    var zodiacLabel: UILabel = {
+       let lbl = UILabel()
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        return lbl
+    }()
+
+    var zodiacImage: UIImageView = {
+        let imgView = UIImageView()
+        imgView.translatesAutoresizingMaskIntoConstraints = false
+        imgView.contentMode = .scaleAspectFit
+        return imgView
+    }()
     
+    var stackView : UIStackView = {
+        var stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.distribution = .fill
+        stack.axis = .horizontal
+        stack.alignment = .fill
+        stack.spacing = 2
+        return stack
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        stackView.addArrangedSubview(zodiacLabel)
+        stackView.addArrangedSubview(zodiacImage)
+        contentView.addSubview(stackView)
+        setupConstraints()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+    func setupConstraints(){
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+
+            zodiacImage.widthAnchor.constraint(equalToConstant: 45),
+            zodiacImage.heightAnchor.constraint(equalToConstant: 45)
+        ])
+        
+    }
 }
